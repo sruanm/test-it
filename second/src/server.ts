8419a431ee81b9cbc38e240b33d2dcc6ce341089
Ruan <santosruan2021@gmail.com>
2026-04-28 14:28:10 -0300

    feat: basic setup finished

---
+++ second/src/server.ts
@@ -0,0 +1,18 @@
+import Express from 'express'
+import { AppDataSource } from './data-source';
+
+async function main() {
+    const app = Express();
+    const PORT = 3001;
+
+    try {
+        await AppDataSource.initialize();
+    } catch (err) {
+        console.error(err);
+        return;
+    }
+
+    app.listen(PORT, () => console.info(`Server up on port ${PORT}!`))
+}
+
+main();
